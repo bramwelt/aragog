7603247194001c719ea3f109a691ff8f3c5c69b6
Trevor Bramwell <trevor@bramwell.net>
2014-08-08 09:01:59 -0700

    Match uri and return 404 response if not found.
    Minor pep8 fixes as well.

--- aragog/routing/decorator.py
+++ aragog/routing/decorator.py
@@ -7,10 +7,15 @@ Convert any function into a WSGI endpoint with a simple decorator.
 """
 
 from wsgiref.simple_server import make_server
+from urlparse import urlsplit
 
-#def debug_env(env):
-#    for k, v in env.iteritems():
-#        yield "{0}: {1}\n".format(k, v)
+
+def endpoint404(environ, start_response):
+    status = '404 NOTFOUND'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    return []
+
 
 def route(uri):
     """
@@ -19,20 +24,22 @@ def route(uri):
     :param: uri
     :param_type: string
     """
-
     def app_wrapper(f):
         def app(environ, start_response):
-            for k,v in environ.iteritems():
-                print " + {0}: {1}".format(k, v)
-            return f(environ, start_response)
+            url = environ.get('PATH_INFO', '')
+            path = urlsplit(url).path
+            if uri == path:
+                return f(environ, start_response)
+            return endpoint404(environ, start_response)
         return app
     return app_wrapper
+
 
 @route("/")
 def simple_app(environ, start_response):
     """Simplest possible application object"""
     status = '200 OK'
-    response_headers = [('Content-type','text/plain')]
+    response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     return ["hello, world!\n"]
 

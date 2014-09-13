0a05326fb122b90e8acca9b619cb0cab0d4961aa
Trevor Bramwell <trevor@bramwell.net>
2014-09-13 14:00:11 -0700

    Expose decorator router at aragog.Router

--- aragog/__init__.py
+++ aragog/__init__.py
@@ -14,3 +14,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 __version__ = ("0", "1", "0")
+
+from aragog.routing.decorator import Router

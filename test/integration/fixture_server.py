0383ee9511a4b002fbb3c00b3fc6812e8cc6bf1e
Trevor Bramwell <trevor@bramwell.net>
2014-09-13 14:04:08 -0700

    Update fixture server with environ route.
    Minor changes to docstrings and change in quotation usage.
    Router imported from aragog, not routing.decorator.

--- test/integration/fixture_server.py
+++ test/integration/fixture_server.py
@@ -4,29 +4,49 @@ Tests against the URL Router
 
 from wsgiref.simple_server import make_server
 
-from aragog.routing.decorator import Router
+from aragog import Router
 
 
 router = Router()
 
-
-@router.route("/")
+@router.route('/')
 def simple_app(environ, start_response):
-    """Simplest possible application object"""
+    """
+    Simplest possible application object
+    """
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     return ["hello, world!\n"]
 
 
-@router.route("/foo")
+@router.route('/foo')
 def foo_app(environ, start_response):
-    """Foo application. Outputs 'foobar!'"""
+    """
+    Foo application. Outputs 'foobar!'
+    """
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     return ["foobar!\n"]
 
+@router.route('/environ')
+def environ_app(environ, start_response):
+    """
+    Outputs the full server environ passed in to the request
+    """
+    response_body = ['%s: %s' % (key, value)
+                     for key, value in sorted(environ.items())]
+    response_body = '\n'.join(response_body)
+
+    status = '200 OK'
+    response_headers = [
+        ('Content-Type', 'text/plain'),
+        ('Content-Length', str(len(response_body)))
+    ]
+    start_response(status, response_headers)
+
+    return [response_body]
 
 if __name__ == "__main__":
     httpd = make_server('', 8080, router)
